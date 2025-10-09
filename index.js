34d982c4fdc887e767e66e0e4fe2f80003a8ec4c
munsifkhandev <munsifkhandev@gmail.com>
2025-10-09 11:13:53 +0500

    Done with the Basic Setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 6655;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send(
+    "Congrats your Expense Management System API is working Perfectly.."
+  );
+});
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`);
+});
